388bc128917e13b2bf0b1717bc16765ccda0c026
Lin Yuan <apeforest@gmail.com>
2014-07-06 18:28:52 -0700

    initial commit

---
+++ src/com/codepath/simpletipcalculator/MainActivity.java
@@ -0,0 +1,47 @@
+package com.codepath.simpletipcalculator;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends Activity {
+	private EditText totalAmountText;
+	private TextView tipValueText;
+	private double totalAmount = 0.0;
+	private double tipValue = 0.0;
+	
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        totalAmountText = (EditText) findViewById(R.id.total_amount_label);
+        tipValueText = (TextView) findViewById(R.id.tip_amount_label);
+    }
+    
+    public void OnTenPercent(View v) {
+        if(!(totalAmountText.getText().toString()).isEmpty())
+        	totalAmount = Double.parseDouble(totalAmountText.getText().toString());
+
+    	tipValue = totalAmount * 0.1;
+        tipValueText.setText(String.format("%.2f", tipValue));
+    }
+    
+    public void OnFifteenPercent(View v) {
+        if(!(totalAmountText.getText().toString()).isEmpty())
+        	totalAmount = Double.parseDouble(totalAmountText.getText().toString());
+
+    	tipValue = totalAmount * 0.15;
+        tipValueText.setText(String.format("%.2f", tipValue));
+    }
+    
+    public void OnTwentyPercent(View v) {
+        if(!(totalAmountText.getText().toString()).isEmpty())
+        	totalAmount = Double.parseDouble(totalAmountText.getText().toString());
+
+    	tipValue = totalAmount * 0.2;
+        tipValueText.setText(String.format("%.2f", tipValue));
+    }
+    
+}
